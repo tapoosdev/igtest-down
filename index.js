e6f64894295b20a95fb89f2211d9200de959df4c
tapoosdev <133811283+tapoosdev@users.noreply.github.com>
2023-11-15 03:30:41 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,18 @@
-var slideIndex = 0;
+import React from "react";
+import ReactDOM from "react-dom";
 
-showSlide();
+import App from "./App";
+import { HashRouter } from 'react-router-dom';
 
-// var timer = setInterval(showSlide, 2000);
+ReactDOM.render(
 
-var slideContainer = document.getElementById('slideshow-container');
-var slideControl = document.getElementById('slide-control');
+    
 
-slideContainer.onmouseover = function() {
-	clearInterval(timer);
-}
+    <HashRouter>
 
-slideControl.onmouseover = function() {
-	clearInterval(timer);
-}
+        <App />
 
-slideContainer.onmouseleave = function() {
-	// timer = setInterval(showSlide, 2000);
-}
+    </HashRouter>,
 
-slideControl.onmouseleave = function() {
-	// timer = setInterval(showSlide, 2000);
-}
-
-function showSlide() {
-	var i;
-	var slides = document.getElementsByClassName('slide-item');
-	var dots = document.getElementsByClassName('dot');
-	for (var i = 0; i < slides.length; i++) {
-		slides[i].style.display = 'none';
-	}
-	for (var i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
-	slideIndex++;
-	if (slideIndex > slides.length) slideIndex = 1;
-	slides[slideIndex-1].style.display = "block";
-	dots[slideIndex-1].className += " active";
-}
-
-function chooseSlide(n) {
-	slideIndex = n;
-	showSlide();
-	clearInterval(timer);
-}
-
-function showMenu() {
-	var topNav = document.getElementById('topnav');
-	if (topNav.className === "navbar") {
-		topNav.className += " show";
-	} else {
-		topNav.className = "navbar";
-	}
-}
+    document.getElementById("root")
+);
